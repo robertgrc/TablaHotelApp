04941196f2702939d8780ec866e3039cbedb8aac
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-04-19 14:36:49 -0400

    Planning de Reservaciones

--- src/TablaCalendarioReservas/TablaCalendarioReservas.js
+++ src/TablaCalendarioReservas/TablaCalendarioReservas.js
@@ -12,11 +12,18 @@ function TablaCalendarioReservas() {
       { id: 5, nombre: '05 TWB', numero: '105' },
       { id: 6, nombre: '06 SWB', numero: '106' },
       { id: 7, nombre: '07 TWB', numero: '107' },
+      { id: 8, nombre: '08 TWB', numero: '108' },
+      { id: 9, nombre: '09 TWB', numero: '109' },
+      { id: 10, nombre: '10 SWB', numero: '110' },
+      { id: 11, nombre: '11 TWB', numero: '111' },
+      { id: 12, nombre: '12 DWB', numero: '112' },
+      { id: 13, nombre: '13 SWB', numero: '113' },
+      { id: 14, nombre: '14 TWB', numero: '114' },
   ];
 
   const reservas = [
     {
-      id: 4,                  
+      id: 1,                  
       habitacion: '101',      
       nombre: 'Juan Pérez',   
       fechaReserva: '2023-04-20',  
@@ -27,20 +34,56 @@ function TablaCalendarioReservas() {
     },
     {
       id: 2,
-      habitacion: '102',
+      habitacion: '101',
       nombre: 'julio',
-      fechaReserva: '2023-04-20', 
-      fechaInicio: '2022-04-26',
-      fechaFin: '2022-04-28',
+      fechaReserva: '2023-04-3', 
+      fechaInicio: '2023-04-15',
+      fechaFin: '2023-04-18',
       estado: 'provisional'
     },
     {
       id: 3,
       habitacion: '103',
-      nombre: '03 DWB',
+      nombre: 'max',
       fechaReserva: '2023-04-20', 
-      fechaInicio: '2022-04-22',
-      fechaFin: '2022-04-23',
+      fechaInicio: '2023-04-22',
+      fechaFin: '2023-04-23',
+      estado: 'cancelado'
+    },
+    {
+      id: 4,
+      habitacion: '104',
+      nombre: 'dax',
+      fechaReserva: '2023-04-20', 
+      fechaInicio: '2023-04-8',
+      fechaFin: '2023-04-14',
+      estado: 'confirmado'
+    },,
+    {
+      id: 5,
+      habitacion: '105',
+      nombre: 'flor',
+      fechaReserva: '2023-04-4', 
+      fechaInicio: '2023-04-7',
+      fechaFin: '2023-04-23',
+      estado: 'provisional'
+    },
+    {
+      id: 6,
+      habitacion: '111',
+      nombre: 'ken',
+      fechaReserva: '2023-04-1', 
+      fechaInicio: '2023-04-1',
+      fechaFin: '2023-04-4',
+      estado: 'confirmado'
+    },
+    {
+      id: 7,
+      habitacion: '114',
+      nombre: 'Ryu',
+      fechaReserva: '2023-04-28', 
+      fechaInicio: '2023-04-28',
+      fechaFin: '2023-04-30',
       estado: 'cancelado'
     }
   ];
--- src/TablaCalendarioReservas/TablaReservas.js
+++ src/TablaCalendarioReservas/TablaReservas.js
@@ -15,39 +15,45 @@ function TablaReservas({ habitaciones, diasDelMes, mesActualNumerico, yearActual
         </tr>
       </thead>
       <tbody>
-        {habitaciones.map((habitacion) => (
-          <tr key={habitacion.id}>
-            <td>{habitacion.nombre}</td>
-            {[...Array(diasDelMes)].map((_, i) => {
-              const fecha = new Date(yearActual, mesActualNumerico - 1, i + 1);
-              const reservasHabitacion = reservas.filter(reserva => reserva.habitacion === habitacion.numero);
-              const reservaDia = reservasHabitacion.find(reserva => fecha.getTime() >= new Date(reserva.fechaInicio).getTime() && fecha.getTime() <= new Date(reserva.fechaFin).getTime());
-              let color = 'white';
-              if (reservaDia) {
-                switch (reservaDia.estado) {
-                  case 'alquilado':
-                    color = 'red';
-                    break;
-                  case 'confirmado':
-                    color = 'green';
-                    break;
-                  case 'provisional':
-                    color = 'yellow';
-                    break;
-                  case 'cancelado':
-                    color = 'gray';
-                    break;
-                  default:
-                    break;
+        {habitaciones.map((habitacion) => {
+          const reservasHabitacion = reservas.filter(reserva => reserva.habitacion === habitacion.numero);
+          return (
+            <tr key={habitacion.id}>
+              <td>{habitacion.nombre}</td>
+              {[...Array(diasDelMes)].map((_, i) => {
+                const fecha = new Date(yearActual, mesActualNumerico - 1, i + 1);
+                const reservaDia = reservasHabitacion.find(reserva => fecha.getTime() >= new Date(reserva.fechaInicio).getTime() && fecha.getTime() <= new Date(reserva.fechaFin).getTime());
+                let color = 'white';
+                let texto = '';
+                if (reservaDia) {
+                  switch (reservaDia.estado) {
+                    case 'alquilado':
+                      color = 'red';
+                      texto = reservaDia.nombre;
+                      break;
+                    case 'confirmado':
+                      color = 'green';
+                      texto = reservaDia.nombre;
+                      break;
+                    case 'provisional':
+                      color = 'yellow';
+                      texto = reservaDia.nombre;
+                      break;
+                    case 'cancelado':
+                      color = 'grey';
+                      texto = reservaDia.nombre;
+                      break;
+                    default:
+                      color = 'white';
+                  }
                 }
-              }
-              return <td key={i} style={{ backgroundColor: color }}></td>;
-            })}
-          </tr>
-        ))}
+                return <td key={i} style={{ backgroundColor: color }}>{texto}</td>;
+              })}
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
 }
-
 export default TablaReservas;
